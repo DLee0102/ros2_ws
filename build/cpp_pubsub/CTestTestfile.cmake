df11fecfab8b1542f031ac15d68b04880d49009a
DLee <1225390750@qq.com>
2023-05-22 01:07:52 +0800

    初始化

---
+++ build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dl/ros2_ws/src/cpp_pubsub
+# Build directory: /home/dl/ros2_ws/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/cpp_srvcli/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dl/ros2_ws/src/cpp_srvcli
+# Build directory: /home/dl/ros2_ws/build/cpp_srvcli
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
